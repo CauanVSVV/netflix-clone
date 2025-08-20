adf4c2f65faa6ab8c17668d15c3ae1ef55888ac8
Cauan VSVV <cauan.valadares.svv@gmail.com>
2025-08-19 23:08:45 -0300

    feat: implement Home page and TitleCards component with movie API integration

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,32 +1,51 @@
-import React, { useEffect, useRef } from 'react'
-import './TitleCards.css'
-import cards_data from '../../assets/cards/Cards_data'
+import React, { useEffect, useRef, useState } from "react";
+import "./TitleCards.css";
+import cards_data from "../../assets/cards/Cards_data";
 
-const TitleCards = ({title, category}) => {
+const TitleCards = ({ title, category }) => {
+  const [apiData, setApiData] = useState([]);
   const cardsRef = useRef();
 
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNDc3MDFkZTlmNzVkMmUyM2VjZWEzZTYxY2NlZmQ0NSIsIm5iZiI6MTc1NTY1NDMyNS44ODEsInN1YiI6IjY4YTUyOGI1ZDBiZDgwOTZhNGQ1NDlkMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lXPUWrRYrDxhH8OgfdzhSiur3C_L8LnI6dtr7jar848",
+    },
+  };
+
   const handleWheel = (event) => {
-      event.preventDefault();
-      cardsRef.current.scrollLeft += event.deltaY;
-  }
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  };
 
   useEffect(() => {
-      cardsRef.current.addEventListener('wheel', handleWheel);
+    fetch(
+      `https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`,
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setApiData(res.results))
+      .catch((err) => console.error(err));
+    cardsRef.current.addEventListener("wheel", handleWheel);
   }, []);
-  
+
   return (
     <div className="title-cards">
-      <h2>{title?title: 'Popular on Netflix'}</h2>
+      <h2>{title ? title : "Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index) => {
-          return <div className="card" key={index}>
-            <img src={card.image} alt="" />
-            <p>{card.name}</p>
-          </div>
+        {apiData.map((card, index) => {
+          return (
+            <div className="card" key={index}>
+              <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="" />
+              <p>{card.original_title}</p>
+            </div>
+          );
         })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default TitleCards
+export default TitleCards;
