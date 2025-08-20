1df4017c4604211aa5a869d1c99c0987cb229c85
Cauan VSVV <cauan.valadares.svv@gmail.com>
2025-08-19 22:56:33 -0300

    feat: implement login page with authentication UI and routing setup

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,12 +1,46 @@
-import React from 'react'
-import './Login.css'
+import React from "react";
+import "./Login.css";
+import logo from "../../assets/logo.png";
 
 const Login = () => {
+  const [signState, setSignState] = React.useState("Sign In");
+
   return (
     <div className="login">
-      
+      <img src={logo} className="login-logo" alt="" />
+      <div className="login-form">
+        <h1>{signState}</h1>
+        <form>
+          {signState === "Sign Up" ? (
+            <input type="text" placeholder="Your name" />
+          ) : (
+            <></>
+          )}
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button type="submit">{signState}</button>
+          <div className="form-help">
+            <div className="remenber">
+              <input type="checkbox" />
+              <label htmlFor="">Remember me</label>
+            </div>
+            <p>Need help?</p>
+          </div>
+        </form>
+        <div className="form-switch">
+          {signState === "Sign In" ? (
+            <p>
+              New to Netflix? <span onClick={() => setSignState("Sign Up")}>Sign Up Now</span>
+            </p>
+          ) : (
+            <p>
+              Already have an account? <span onClick={() => setSignState("Sign In")}>Sign In</span>
+            </p>
+          )}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
