3ce98f5665e8728a216b8779804e89a3c62eaf94
Cauan VSVV <cauan.valadares.svv@gmail.com>
2025-08-22 00:53:59 -0300

    feat: create Home page with hero banner, title cards and responsive styling

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -24,10 +24,10 @@ const Home = () => {
                 <button className='btn'><img src={play_icon} alt="" />Play</button>
                 <button className='btn dark-btn'><img src={info_icon} alt="" />More Info</button>
             </div>
-            <TitleCards title="Popular on Netflix" />
         </div>
       </div>
       <div className="more-cards">
+        <TitleCards title="Popular on Netflix" />
         <TitleCards title={"Blockbuster movies"} category="top_rated"/>
         <TitleCards title={"Only on Netflix"} category="popular"/>
         <TitleCards title={"Upcoming"} category="upcoming"/>
