98cdcf4742b97b32a61b6e3f7414778ed6f9f8cc
Cauan VSVV <cauan.valadares.svv@gmail.com>
2025-08-21 23:22:03 -0300

    feat: implement login page with authentication and responsive styling

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,23 +2,33 @@ import React from "react";
 import "./Login.css";
 import logo from "../../assets/logo.png";
 import { login, signup } from "../../firebase";
+import netflix_spinner from "../../assets/netflix_spinner.gif";
 
 const Login = () => {
   const [signState, setSignState] = React.useState("Sign In");
   const [name, setName] = React.useState("");
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
+  const [loading, setLoading] = React.useState(false);
 
   const user_auth = async (event) => {
     event.preventDefault();
+    setLoading(true);
+
     if (signState === "Sign In") {
       await login(email, password);
     } else {
       await signup(name, email, password);
     }
+
+    setLoading(false);
   };
 
-  return (
+  return loading ? (
+    <div className="login-spinner">
+      <img src={netflix_spinner} alt="" />
+    </div>
+  ) : (
     <div className="login">
       <img src={logo} className="login-logo" alt="" />
       <div className="login-form">
@@ -27,7 +37,9 @@ const Login = () => {
           {signState === "Sign Up" ? (
             <input
               value={name}
-              onChange={(e) => {setName(e.target.value)}}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
               type="text"
               placeholder="Your name"
             />
@@ -36,13 +48,17 @@ const Login = () => {
           )}
           <input
             value={email}
-            onChange={(e) => {setEmail(e.target.value)}}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
             type="email"
             placeholder="Email"
           />
           <input
             value={password}
-            onChange={(e) => {setPassword(e.target.value)}}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
             type="password"
             placeholder="Password"
           />
